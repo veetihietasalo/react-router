8f9282d56795af2aa64b8151ab1fe9963f10d226
Venomity <veeti.hietasalo@hotmail.fi>
2023-10-11 13:45:14 +0300

    react-router palautus

---
+++ src/main.jsx
@@ -0,0 +1,36 @@
+import './index.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import App from './App';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        element: <Home />,
+        index: true
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "contact",
+        element: <Contact />,
+      },
+    ]
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+);
